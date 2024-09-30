b5bda4b4db8126fba3dd42a123f1aecf40858eaf
Md-Junaead <junaead12364@gmail.com>
2024-09-30 22:33:26 +0600

    V5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,40 @@
-import 'package:flutter/material.dart';
-import 'package:v3/App/app.dart';
+//Flutter Bangla
+import "package:flutter/material.dart";
 
-void main() {
+main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.green),
+      color: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: const HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Inventory App"),
+        titleSpacing: 0,
+        centerTitle: true,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        backgroundColor: Colors.green,
+        elevation: 6,
+      ),
+    );
+  }
 }

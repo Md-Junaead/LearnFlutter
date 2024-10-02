48e257af90cc849b1dc2af805645138f2a41963c
Md-Junaead <junaead12364@gmail.com>
2024-10-02 22:54:38 +0600

    Navigation Bar & AppBar set

---
+++ lib/git.dart
@@ -0,0 +1,24 @@
+/*
+
+echo "# LearnFlutter" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Md-Junaead/LearnFlutter.git
+git push -u origin main
+
+
+https://github.com/Md-Junaead/LearnFlutter.git
+
+*/
+
+//git push --set-upstream origin v4
+
+/*
+
+git remote add origin https://github.com/Md-Junaead/LearnFlutter.git
+git branch -M main
+git push -u origin main
+
+*/
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.green),
       darkTheme: ThemeData(primarySwatch: Colors.green),
-      color: Colors.blue,
+      color: Colors.white,
       debugShowCheckedModeBanner: false,
       home: const HomeActivity(),
     );
@@ -23,17 +23,58 @@ class MyApp extends StatelessWidget {
 class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
 
+  MySnackBar(message, context) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Message"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "User"),
+        ],
+      ),
       appBar: AppBar(
         title: const Text("Inventory App"),
-        titleSpacing: 0,
-        centerTitle: true,
+        titleSpacing: 10,
         toolbarHeight: 60,
         toolbarOpacity: 1,
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.deepOrange,
         elevation: 6,
+        actions: [
+          IconButton(
+              onPressed: () {
+                MySnackBar("I am Comment", context);
+              },
+              icon: const Icon(Icons.comment)),
+          IconButton(
+              onPressed: () {
+                MySnackBar("I am Search", context);
+              },
+              icon: const Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                MySnackBar("I am Setting", context);
+              },
+              icon: const Icon(Icons.settings)),
+          IconButton(
+              onPressed: () {
+                MySnackBar("I am Email", context);
+              },
+              icon: const Icon(Icons.email)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.deepOrange,
+        onPressed: () {
+          MySnackBar("I am tonni's one and only jamai", context);
+        },
       ),
     );
   }
